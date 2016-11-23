6899b3e62b91b707d7febd412c35a8fb028a3cf4
Alexander Levin <alevin.ru@gmail.com>
2016-11-23 21:15:37 +0200

    get default date and pool

--- index.js
+++ index.js
@@ -5,11 +5,30 @@ const _ = require('lodash');
 
 module.exports.handler = handler;
 
+function getAlias(context) {
+
+  let name = _.get(context, 'functionName');
+  let arn = _.get(context, 'invokedFunctionArn');
+
+  if (name && arn) {
+    return _.last(arn.match(new RegExp(name+':(.*)')));
+  }
+
+}
+
+function getYestergay() {
+  let date = new Date(new Date().setDate(new Date().getDate()-1));
+  return date.substr(0,10);
+}
+
 function handler(event, context, callback) {
 
-  const date = _.isString(event) ? event : '2016-11-09';
+  const date = _.isString(event) ? event : (event.date || getYesterday());
 
-  return service.run(date)
+  console.log('Handler start with event:', event);
+  console.log('Handler start with context:', context);
+
+  return service.run(date, getAlias(context))
     .then(result => callback(null, result))
     .catch(err => callback);
 
